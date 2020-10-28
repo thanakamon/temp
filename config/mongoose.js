4087278d41e0e69e7f86159bcd7a7a066271e0cb
thanakamon <thanakamonmay@gmail.com>
2020-10-28 16:22:13 +0700

    add true

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = function () {
-    mongoose.set('debug', config.debug);
+    mongoose.set('debug',true);
     var db = mongoose.connect('mongodb://localhost/s60030031');
     require('../app/models/temp.model');
     return db;
